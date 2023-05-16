6010aabc771600735c6f9c05651d85d62102e362
gatwirival <valegjohn@gmail.com>
2023-05-16 12:53:28 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>Hello, esbuild! </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
